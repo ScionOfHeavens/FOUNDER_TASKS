1740ad2a829f43e6b3ef99524b874def740172d5
Danila Krupnov <ditrihkruger515@gmail.com>
2025-01-23 23:32:50 +0200

    User interface was added(UI)

--- SchemeUML.md
+++ SchemeUML.md
@@ -10,10 +10,28 @@ class Deck{
     get_card(index: int) -> Card
 }
 
+class User{
+    id: int
+    quistion_id: int
+}
+
+class UserDB{
+    __users: list[User]
+    get_quiz_index(self, user_index: int) -> None
+    update_quiz_index(self, user_index: int, quest_index: int) -> None
+}
+
+class UI{
+    start_quiz() -> None
+    take_answere(self, options: dict) -> str
+    print_card(self, question_number, question, options) -> None
+}
+
 class Quiz{
     
 }
 
 Deck o-- Card
+UserDB o-- User
 
 @enduml
--- quiz.py
+++ quiz.py
@@ -1,20 +1,29 @@
+from random import shuffle
+
 class Card:
     __question: str
-    __answer_options: list[str]
+    __false_options: list[str]
+    __true_option: str
 
     def __init__(self, d: list[str]):
-        self.__answer_options = d["options"]
-        self.__question = d["q"]
+        self.__false_options = d["false_options"]
+        self.__true_option = d["true_option"]
+        self.__question = d["question"]
 
     @property
     def question(self)->str:
         return self.__question
     @property
-    def answer_options(self)->list[str]:
-        return self.__answer_options.copy()
+    def false_option(self) -> list[str]:
+        return self.__false_options.copy()
+    
+    @property
+    def true_option(self) -> str:
+        return self.__true_option
 
     def is_true_option(self, option:str) -> bool:
-        return self.answer_options[0] == option
+        return self.__true_option == option
+
 
 class Deck:
     __cards: list[Card]
@@ -24,7 +33,57 @@ class Deck:
     def get_card(self, index: int) -> Card:
         return self.cards[index]
 
+
+class User:
+    __id: int
+    __quistion_id: int
+    def __init__(self, id: int, question: int) -> None:
+        self.__id = id
+        self.__question_id = question
+    @property
+    def id(self):
+        return self.__id
+    @property
+    def question_id(self) -> None:
+        return self.__question_id
+
+
+class UserDB:
+    __users: list[User]
+    def __init__(self) -> None:
+        self.__users = []
+    def get_quiz_index(self, user_index: int) -> None:
+        return self.__users[user_index]
+    def increment_quiz_index(self, user_index: int) -> None:
+        question_id = self.__users[user_index].question_id
+        self.__users[user_index] = User(user_index, question_id + 1)
+    def update_quiz_index(self, user_index: int, question_index: int) -> None:
+        self.__users[user_index] = User(user_index, question_index)
+
+
+class UI:
+    def start_quiz() -> None:
+        print("Добро пожаловать в Quiz")
+    
+    def print_card(self, question_number, question, options) -> None:
+        print(f"Вопрос номер {question_number}:")
+        print(question)
+        # self.__options = self.__current_card.__false_options + [self.__current_card.true_option]
+        # shuffle(self.__options)
+        print("Варианты ответов:")
+        for number, option in dict(zip(range(1,16),options)):
+            print(f"{number}) {option}")
+
+    def take_answere(self, options: dict) -> str:
+        answere = input()
+        if answere in options.keys():
+            return options[answere]
+        if answere in options.values():
+            return answere
+        print("Enter valid answere, please.")
+        return self.take_answere()
+
+
 class QuizApp:
     __deck: Deck = Deck()
-    def get_card(self, index: int) -> Card:
-        return self.__deck.get_card(index)
+    __userDB: UserDB = UserDB()
