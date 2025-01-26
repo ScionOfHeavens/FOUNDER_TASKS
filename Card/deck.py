07d2178b8198933096e25d7064c4e4b512742ab9
Danila Krupnov <ditrihkruger515@gmail.com>
2025-01-27 00:34:50 +0200

    Json added From card in python dict to card in json

--- Card/deck.py
+++ Card/deck.py
@@ -1,31 +1,14 @@
+import json
 from Card.card import Card
 
 class Deck:
     __cards: dict[int, Card]
+
     def __init__(self):
-        db = [
-            {
-                'question': 'Что такое Python?',
-                'false_options': ['Тип данных', 'Музыкальный инструмент', 'Змея на английском'],
-                'true_option': 'Язык программирования'
-            },
-            {
-                'question': 'Какой тип данных используется для хранения целых чисел?',
-                'false_options': ['float', 'str', 'natural'],
-                'true_option': 'int'
-            },
-            {
-                'question': 'Что такое лямбда-функция?',
-                'false_options': ['функция с именем лямбда', 'функция класса', 'переменная с функцей'],
-                'true_option': 'функция без имени'
-            },
-            {
-                'question': 'Чего больше всего желает автор Квиза?',
-                'false_options': ['Делать ботов', 'Программировать', 'Делать игры'],
-                'true_option': 'Научиться архитектуре'
-            },
-        ]
-        self.__cards = {i: Card(i,e) for i,e in zip(range(1, len(db)+1), db)}
+        data: str
+        with open(r"Card\deck.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+        self.__cards = {i+1: Card(i+1,e) for i,e in enumerate(data)}
 
     async def get_card(self, index: int) -> Card:
         return self.__cards[index]
